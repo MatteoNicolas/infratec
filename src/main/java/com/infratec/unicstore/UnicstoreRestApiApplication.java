646314c2e69f3a364940b25a0a3349283dbe4ecb
MatteoNicolas <matteonicolasjob@gmail.com>
2021-09-14 22:58:58 -0300

    first commit

---
+++ src/main/java/com/infratec/unicstore/UnicstoreRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.infratec.unicstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicstoreRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicstoreRestApiApplication.class, args);
+	}
+
+}
