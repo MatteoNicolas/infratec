127c90e6d6a2684087ba931588de7754def36b0c
MatteoNicolas <matteonicolasjob@gmail.com>
2021-09-15 00:58:32 -0300

    REST delete and update created

--- src/main/java/com/infratec/unicstore/controller/UsuarioController.java
+++ src/main/java/com/infratec/unicstore/controller/UsuarioController.java
@@ -1,10 +1,14 @@
 package com.infratec.unicstore.controller;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +24,7 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioRepository repository;
 	
+	//para encontrar por um parâmetro específico (neste caso é o ID)
 	@GetMapping(path = "/api/usuario/{id}")
 	public ResponseEntity<?> consultar (@PathVariable("id") Integer id) {
 		return repository.findById(id)
@@ -31,4 +36,15 @@ public class UsuarioController {
 	public UsuarioModel salvar(@RequestBody UsuarioModel usuario){
 		return repository.save(usuario);
 	}
+	
+	@DeleteMapping(path = "/api/usuario")
+	public void deletar(@RequestBody UsuarioModel usuario) {
+		repository.delete(usuario);
+	}
+	
+	@PutMapping(path = "/api/usuario")
+	public UsuarioModel atualizar(@RequestBody UsuarioModel usuario) {
+		return repository.save(usuario);
+	}
+	
 }
